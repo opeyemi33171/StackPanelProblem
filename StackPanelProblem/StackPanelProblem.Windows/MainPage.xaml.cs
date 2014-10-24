09a3107db3ab9ca8008e766e2ba3098cc8571917
opeyemi33171 <opeyemi33171@gmail.com>
2014-10-24 03:18:58 -0700

    update 1.0

--- StackPanelProblem/StackPanelProblem.Windows/MainPage.xaml.cs
+++ StackPanelProblem/StackPanelProblem.Windows/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +28,29 @@ namespace StackPanelProblem
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        string[] input;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            input = entryBar.Text.Split(',');
+            PanelItems rect = new PanelItems() {Width=holder.ActualWidth,Height=30 };
+            switch (input[0].ToLower())
+            {
+                case "red":
+                    rect.grid.Background= new SolidColorBrush(Colors.Red);
+                    rect.name.Text = input[1];
+                    break;
+                case "blue":
+                    rect.grid.Background = new SolidColorBrush(Colors.Blue);
+                    rect.name.Text = input[1];
+                    break;
+                case "green":
+                    rect.grid.Background = new SolidColorBrush(Colors.Green);
+                    rect.name.Text = input[1];
+                    break;
+            }
+            holder.Children.Add(rect);
+            
         }
     }
 }
